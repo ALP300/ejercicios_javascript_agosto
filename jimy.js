3bd542ede6b9b5c2ecc80801a4e393a510e38e01
JIMYLITANO <jimto.litano@gmail.com>
2026-08-25 21:33:48 -0500

    ejercicio 02

--- jimy.js
+++ jimy.js
@@ -1,1 +1,16 @@
 console.log("Hola soy Jimy")
+
+
+let usuario = prompt("Ingrese su usuario: ")
+let password = prompt("Ingrese su contraseña: ")
+let rol = prompt("Ingrese su rol: ")
+
+if (usuario == "admin" && password == "admin" && rol == "admin") {
+    console.log("Bienvenido admin")
+} else if (usuario == "editor" && password == "editor" && rol == "editor") {
+    console.log("Bienvenido editor")
+} else if (usuario == "visitante" && password == "visitante" && rol == "visitante") {
+    console.log("Bienvenido visitante")
+} else {
+    console.log("Usuario o contraseña incorrectos")
+}
